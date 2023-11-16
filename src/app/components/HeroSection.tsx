5d89bdf044075265d1576f2530c00eaeb2610cae
PinkPaca <fmadkdl113@gmail.com>
2023-11-16 23:41:11 +0000

    responsive design adjust

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -8,9 +8,9 @@ type Props = {};
 const HeroSection = (props: Props) => {
   return (
     <section>
-      <div className="sm:w-[310px] grid gird-cols-1 md:grid-cols-12 lg:grid-cols-12">
-        <div className="lg:col-span-7 md:col-span-7 mx-10">
-          <h1 className="text-primary-color font-serif sm:text-center mb-4 text-6xl font-extrabold">
+      <div className="grid gird-cols-1 md:grid-cols-12 lg:grid-cols-12">
+        <div className="md:col-span-7 mx-10">
+          <h1 className="text-primary-color font-serif text-center lg:text-start mb-4 text-6xl md:text-5xl font-extrabold">
             <span className="">
               Hello, I'm
               <br />
@@ -29,11 +29,11 @@ const HeroSection = (props: Props) => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-primary-color font-serif mg-6 text-lg lg:text-xl text-start">
+          <p className="text-primary-color font-serif mg-6 text-lg lg:text-xl text-start sm:text-center">
             자기소개쓸거임
           </p>
-          <div className="mt-5 flex justify-evenly">
-            <button className="shadow-pop-tr px-6 py-3 rounded-full mr-4 font-serif font-bold text-quaternary-color border-solid border-2 border-quaternary-color ">
+          <div className="my-5 flex lg:flex-row justify-evenly flex-col ">
+            <button className="shadow-pop-tr px-6 py-3 rounded-full mr-0 mb-3 lg:mb-0 lg:mr-4 font-serif font-bold text-basic-color bg-quaternary-color border-solid border-2 border-quaternary-color ">
               Hire Me
             </button>
             <button className="shadow-pop-tr px-6 py-3 rounded-full font-serif font-bold text-quaternary-color border-solid border-2 border-quaternary-color">
@@ -42,7 +42,7 @@ const HeroSection = (props: Props) => {
           </div>
         </div>
         <div className="lg:col-span-5 md:col-span-5 mx-auto flex justify-center lg:mr-8">
-          <div className="rounded-full bg-primary-color w-500 h-400 relative">
+          <div className="rounded-full bg-primary-color w-[250px] h-[250px] my-auto">
             <Image
               src="/images/hero-img.png"
               alt="hero image"
